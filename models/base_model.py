f395d08ca949becab481b868af1562cf7e3918fa
mwanyambu <jmwanyambu@gmail.com>
2023-10-14 13:42:48 -0700

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -26,7 +26,8 @@ class BaseModel:
 
     def __str__(self):
         """return string representation of BaseModel"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
         """
