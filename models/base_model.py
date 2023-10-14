f0b84d52775b43f181af765eabd5b28fa6698d72
mwanyambu <jmwanyambu@gmail.com>
2023-10-14 14:01:35 -0700

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -11,7 +11,7 @@ class BaseModel:
     """
     def __init__(self, *args, **kwargs):
         """initialization"""
-        if kwargs:
+        if len(kwargs) != 0:
             for k, v in kwargs.items():
                 if k == '__class__':
                     continue
@@ -22,7 +22,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
             models.storage.new(self)
 
     def __str__(self):
